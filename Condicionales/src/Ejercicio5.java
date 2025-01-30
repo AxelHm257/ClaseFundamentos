ef41bd1729dfc5f9714862bc851a7b1db44814c5
AxelHm257 <123045383@upq.edu.mx>
2025-01-30 15:48:57 -0600

    Ejercicio5

--- Condicionales/src/Ejercicio5.java
+++ Condicionales/src/Ejercicio5.java
@@ -15,23 +15,23 @@ public class Ejercicio5 {
         System.out.println("Digite el operador -, para hacer una resta");
         System.out.println("Digite el operador *, para hacer una multiplicacion");
         System.out.println("Digite el operador /, para hacer una divicion");
-        
+
         String operador = sc.next();
         switch (operador) {
 
             case "+":{
-                int suma=num1+num2;
-                System.out.println("La suma es: "+suma);
+                int suma = num1 + num2;
+                System.out.println("La suma es: "+ suma);
             } break;
             case "-":{
-                int resta=num1-num2;
-                System.out.println("La resta es: "+resta);
+                int resta =num1-num2;
+                System.out.println("La resta es: "+ resta);
             } break;
-            case "*":{int multiplicacion=num1*num2;
-                System.out.println("La multiplicacion es: "+multiplicacion);} break;
+            case "*":{int multiplicacion = num1 * num2;
+                System.out.println("La multiplicacion es: "+ multiplicacion);} break;
 
-            case "/":{int division=num1/num2;
-                System.out.println("La division es: "+division);
+            case "/":{int division = num1 / num2;
+                System.out.println("La division es: "+ division);
             } break;
         }
     }
