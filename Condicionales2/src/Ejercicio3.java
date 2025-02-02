3eea8a69c2d9032ad43482384888d3b115ae21be
AxelHm257 <123045383@upq.edu.mx>
2025-02-01 20:27:22 -0600

    Ejercicio3

---
+++ Condicionales2/src/Ejercicio3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i+ "X" + i + " = " + i*i);
+        }
+    }
+}
